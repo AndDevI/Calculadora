b7a117d9754891b2bc2f393cfc30ba53263b96a3
AndrewRaphael10 <andrewrgpires@gmail.com>
2024-02-05 17:36:10 -0300

    add-bolean

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-let buttons = document.querySelector('.buttons')
-let btn = document.querySelectorAll('span')
-let value = document.getElementById('value')
+let buttons = document.querySelector(".buttons")
+let btn = document.querySelectorAll("span")
+let value = document.getElementById("value")
+let numberClicked = false
 
-  for(let i=0; i<btn.length;i++){
-    btn[i].addEventListener("click", function(){
-
-      if(this.innerHTML=="="){
-        value.innerHTML = eval(value.innerHTML);
-      }   else {
-            if(this.innerHTML=="clear"){
-             value.innerHTML = ""; 
-            } else {
-                value.innerHTML += this.innerHTML;
-            }
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", function () {
+    if (this.innerHTML === "=") {
+      if (numberClicked) {
+        value.innerHTML = eval(value.innerHTML)
+        numberClicked = false
       }
-
-    })
-  }
+    } else if (this.innerHTML.trim().toLowerCase() === "clear") {
+      value.innerHTML = ""
+      numberClicked = false
+    } else if (/[0-9]/.test(this.innerHTML)) {
+      value.innerHTML += this.innerHTML
+      numberClicked = true
+    } else {
+      if (numberClicked) {
+        value.innerHTML += this.innerHTML
+        numberClicked = false
+      }
+    }
+  })
+}
